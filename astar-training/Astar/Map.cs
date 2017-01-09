426ca17d57e68b137ccb8c7253a2ea9e12492903
Vincent Dumont <dumontv@outlook.com>
2017-01-08 21:19:20 -0500

    added basic map class

---
+++ astar-training/Astar/Map.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace astar_training.Astar
+{
+    class Map
+    {
+        public enum Tile { Ground, Wall, Entry, Exit };
+        private Tile[,] map;
+
+        public Map(int preset = 1)
+        {
+            switch (preset)
+            {
+                case 1: default:
+                    map = createMapPreset1();
+                    break;
+            }
+        }
+
+        private Tile[,] createMapPreset1()
+        {
+            return new Tile[10,10] {
+                { Tile.Entry,  Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground },
+                { Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground },
+                { Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground },
+                { Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground },
+                { Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground },
+                { Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground },
+                { Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground },
+                { Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground },
+                { Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground },
+                { Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Ground, Tile.Exit }
+            };
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    switch (map[i, j])
+                    {
+                        case Tile.Ground:
+                            Console.Write("x");
+                            break;
+                        case Tile.Wall:
+                            Console.Write("#");
+                            break;
+                        case Tile.Entry:
+                            Console.Write("B");
+                            break;
+                        case Tile.Exit:
+                            Console.Write("E");
+                            break;
+                    }
+                    if (j == map.GetLength(0) - 1)
+                        Console.Write("\n");
+                }
+            }
+        }
+    }
+}
---
+++ astar-training/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using astar_training.Astar;
+
+namespace astar_training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Map map = new Map(1);
+            map.Draw();
+            Console.ReadKey();
+        }
+    }
+}
